cc23136468af9fb67cc1e25ba49ad36bcd451853
natsu2711 <441517582@qq.com>
2025-06-03 23:52:50 +0800

    Post接口响应和请求

--- src/main.js
+++ src/main.js
@@ -14,6 +14,10 @@ server.listen(3000,()=>{
 })
 */
 
+//使用JSON中间件
+app.use(express.json());
+
+
 app.listen(port,() =>{
     console.log('服务已启动！！！');
 });
@@ -60,3 +64,23 @@ app.get('/posts/:postId',(request,response) =>{
     response.send(posts[0]);
 
 })
+
+//创建内容
+app.post('/posts',(request,response) =>{
+    //获取请求里的数据
+    const { content } = request.body;
+
+    //设置响应状态码
+    response.status(201);
+
+    //输出请求头部数据
+    console.log(request.headers['sing-along']);
+
+    //设置响应的头部数据
+    response.set('Sing-Along','How I wonder what you are!')
+
+    //做出响应
+    response.send({
+        message : `成功创建了内容： ${content}`
+    });
+});
