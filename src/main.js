ba8b2eead856472b8da0fd442cdd91c31a920ae2
natsu2711 <441517582@qq.com>
2025-06-03 20:18:38 +0800

    定义接口参数

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-const http = require('http');
+//const http = require('http');
+const express =require('express');
+const app =express();
+const port = 3000;
 
-const server =http.createServer((request,Response)=>{
+/*const server =http.createServer((request,Response)=>{
     Response.write('hello~~');
     Response.end();
 
@@ -8,4 +11,52 @@ const server =http.createServer((request,Response)=>{
 
 server.listen(3000,()=>{
     console.log('服务已启动！！！');
+})
+*/
+
+app.listen(port,() =>{
+    console.log('服务已启动！！！');
+});
+
+
+app.get('/',(request,response) =>{
+    response.send('来咯');
+});
+
+//准备一组数据
+const data=[
+    {
+        id: 1,
+        title: '关山月',
+        content : '明月出1山，苍茫云海间'
+    },
+        {
+        id: 2,
+        title: '关2月',
+        content : '明月出2山，苍茫云海间'
+    },
+        {
+        id: 3,
+        title: '关3月',
+        content : '明月出3山，苍茫云海间'
+    }
+];
+
+//要给客户端需要转换成json格式，做出响应时需要定义头部信息
+app.get('/posts',(request,response) =>{
+    response.send(data);
+
+})
+
+//定义接口的时候可以定义一些参数，接口处理器（就是那个()=>函数），会根据参数的值决定把什么数据发给客户端
+app.get('/posts/:postId',(request,response) =>{
+    //获取内容ID
+    const{ postId } = request.params;
+
+    //查找具体内容
+    const posts = data.filter(item => item.id == postId);
+
+    //做出响应
+    response.send(posts[0]);
+
 })
